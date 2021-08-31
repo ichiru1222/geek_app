aac448c615d4a15e4555a314f26ef6f1f7368703
ichiru matsuda <ichiru1222@gmail.com>
2021-08-31 19:11:12 +0900

    ゆーざidに紐づいた投稿機能の実装

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -5,4 +5,20 @@ class ContentsController < ApplicationController
     def new
         @content = Content.new
     end
+
+    def create
+        content = Content.new(content_params)
+        content.user_id = current_user.id
+        if content.save
+            redirect_to :action => "index"
+        else
+            redirect_to :action => "new"
+        end
+    end
+
+
+    private
+    def content_params
+        params.require(:content).permit(:title, :artist, :video, :comment)
+    end
 end
