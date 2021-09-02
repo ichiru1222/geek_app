f8fce2d30a789229d0a67d5a26143943e554db37
ichiru matsuda <ichiru1222@gmail.com>
2021-09-02 22:23:46 +0900

    投稿の編集と削除機能の追加

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -21,8 +21,24 @@ class ContentsController < ApplicationController
         @content = Content.find(params[:id])
     end
 
-    
+    def edit
+        @content = Content.find(params[:id])
+    end
 
+    def update
+        content = Content.find(params[:id])
+        if content.update(content_params)
+            redirect_to :action => "show", :id => content.id
+        else
+            redirect_to :action => "new"
+        end
+    end
+
+    def destroy
+        content = Content.find(params[:id])
+        content.destroy
+        redirect_to action: :index
+    end
 
     private
     def content_params
