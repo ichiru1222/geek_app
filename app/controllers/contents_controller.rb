c5e9766a8148bd995cf9ff73bb33ede57c5f83fe
ichiru matsuda <ichiru1222@gmail.com>
2021-10-08 15:16:22 +0900

    ログアウト時のバグ修正

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -7,9 +7,9 @@ class ContentsController < ApplicationController
         if params[:search] != nil && params[:search] != ''
             #部分検索かつ複数検索
             search = params[:search]
-            @contents = Content.joins(:user).where("title LIKE ? OR name LIKE ?", "%#{search}%", "%#{search}%")
+            @contents = Content.joins(:user).where("title LIKE ? OR name LIKE ?", "%#{search}%", "%#{search}%").page(params[:page]).per(3)
           else
-            @contents = Content.all
+            @contents = Content.all.page(params[:page]).per(3)
           end
     end
 
